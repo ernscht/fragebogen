0fd5c4deb9ea720e629e30d29e6544996c666d50
ernscht <ernscht@gmail.com>
2026-04-14 16:50:48 +0200

    js: version srdan

--- js/bmi.js
+++ js/bmi.js
@@ -66,18 +66,71 @@ Qualtrics.SurveyEngine.addOnReady(function() {
         // Clear previous drawing
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw the 5 segments (0 to 4)
-        [0, 1, 2, 3, 4].forEach(function(i) {
-            // Each segment is 1/5th of the semi-circle (PI / 5)
-            var startAngle = Math.PI + (i * (Math.PI / 5));
-            var endAngle = Math.PI + ((i + 1) * (Math.PI / 5));
+
+        // CUSTOM START
+        function degToRad(deg) {
+            return (deg * Math.PI) / 180;
+        }
+
+        function drawRingSegment(centerX, centerY, outerR, innerR, startDeg, endDeg, color) {
+            const start = degToRad(startDeg);
+            const end = degToRad(endDeg);
 
             ctx.beginPath();
-            ctx.arc(centerX, centerY, radius, startAngle, endAngle, false);
-            ctx.lineWidth = 30;
-            ctx.strokeStyle = riskConfig[i].color;
+            ctx.arc(centerX, centerY, outerR, start, end, false);
+            ctx.arc(centerX, centerY, innerR, end, start, true);
+            ctx.closePath();
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
+
+        function drawTachometer() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            const centerX = canvas.width / 2;
+            const centerY = canvas.height * 0.92;
+            const outerR = Math.min(canvas.width * 0.46, canvas.height * 0.85);
+            const innerR = outerR * 0.64;
+
+            const gap = 3.5;
+            const segments = [
+                { start: 180, end: 198, color: '#00961a' },
+                { start: 198 + gap, end: 213, color: '#50bb4a' },
+                { start: 213 + gap, end: 235, color: '#86c64e' },
+                { start: 235 + gap, end: 265, color: '#f4b533' },
+                { start: 265 + gap, end: 360, color: '#ff5730' }
+            ];
+
+            segments.forEach(seg => {
+                drawRingSegment(centerX, centerY, outerR, innerR, seg.start, seg.end, seg.color);
+            });
+
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, outerR + 6, Math.PI, 0, false);
+            ctx.lineWidth = 14;
+            ctx.strokeStyle = '#f2f2f2';
             ctx.stroke();
+        }
+
+        drawTachometer();
+
+        // CUSTOM END
+
+        /*
+        // Draw the 5 segments (0 to 4)
+        [0, 1, 2, 3, 4].forEach(function(i) {
+          // Each segment is 1/5th of the semi-circle (PI / 5)
+          var startAngle = Math.PI + (i * (Math.PI / 5));
+          var endAngle = Math.PI + ((i + 1) * (Math.PI / 5));
+
+          ctx.beginPath();
+          ctx.arc(centerX, centerY, radius, startAngle, endAngle, false);
+          ctx.lineWidth = 30;
+          ctx.strokeStyle = riskConfig[i].color;
+          ctx.stroke();
         });
+        */
+
 
         // Calculate Needle Angle
         // Math.PI is the start (left), we add (score * segmentWidth)
